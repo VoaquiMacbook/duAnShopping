774632a01cf92056881678aee42e8606ad357542
NguyenTranNhatAn <nan22052004@gmail.com>
2024-01-24 22:37:03 +0700

    g

--- App.tsx
+++ App.tsx
@@ -12,10 +12,10 @@ import {
 } from 'react-native';
 import Shop from './src/an/Shop';
 import FlashSale from './src/an/FlashSale';
-import Start1 from './src/apps/main/stacks/authen/Start1';
-import Password from './src/apps/main/stacks/authen/Password';
-import Login from './src/apps/main/stacks/authen/Login';
-import Acccount from './src/apps/main/stacks/authen/Acccount';
+import Start1 from './src/apps/authen/Start1';
+import Password from './src/apps/authen/Password';
+import Login from './src/apps/authen/Login';
+import Acccount from './src/apps/authen/Acccount';
 function App(): React.JSX.Element {
 
   return (
