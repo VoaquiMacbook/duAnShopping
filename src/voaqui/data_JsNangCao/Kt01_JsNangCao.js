7da5ead14598a9dc1ac8f0cc35fd952694804e82
Vo-A-Qui <vohqui51202@icloud.com>
2024-02-28 17:06:38 +0700

    kt_01_jsNangCao

--- src/voaqui/data_JsNangCao/Kt01_JsNangCao.js
+++ src/voaqui/data_JsNangCao/Kt01_JsNangCao.js
@@ -2,33 +2,89 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const Kt01_JsNangCao = () => {
+    // c1
     const ageOne = [5, 2, 4, 1, 15, 8, 3, 10, 20];
     const ageTwo = [16, 6, 10, 5, 6, 1, 4];
-
     const ageAll = [...new Set(ageOne.concat(ageTwo))];
-
     console.log(ageAll);
-    
-    // async function logMovies() {
-    //     const response = await fetch("https://fakestoreapi.com/products?limit=10 ");
-    //     const movies = await response.json();
-    //     console.log(movies);
-    //   }
-    //   logMovies()
-    // async function logSanPham() {
-    //     const response = await fetch("https://fakestoreapi.com/products?limit=10 ");
-    //     const sanpham = await response.json();
 
-    //     // Lọc mảng để tìm các sản phẩm thuộc danh mục "electronics" và "jewelery"
-    //     const electronics = sanpham.filter(sanpham => sanpham.category === "electronics");
-    //     const jewelery = sanpham.filter(sanpham => sanpham.category === "jewelery");
+    // c2
+    const ageArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const ageFinal = ageArray.map(item => {
+        if (item % 2 === 0) {
+            return item;
+        } else {
+            return item ** 2;
+        }
+    });
+    console.log(ageFinal);
 
-    //     // In ra số lượng sản phẩm của mỗi danh mục
-    //     console.log("Số lượng sản phẩm trong danh mục electronics:", electronics.length);
-    //     console.log("Số lượng sản phẩm trong danh mục jewelery:", jewelery.length);
-    // }
+    // c3
+    const ageSort = ageFinal.sort((a, b) => a - b);
+    console.log(ageSort);
 
-    logSanPham();
+    // c4
+    const resultArray = ageOne.length > ageTwo.length ? ageOne : ageTwo;
+    console.log(resultArray);
+
+    // c5
+    let sum = 0;
+    ageFinal.forEach(item => {
+        if (item >= 10) {
+            sum += item;
+        }
+    });
+    console.log(sum);
+
+    // c6 => html
+
+    // c9
+    const Menber = function (name, address, phone, cccd) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.cccd = cccd;
+    }
+
+    class Person {
+        constructor(name, address, phone, cccd) {
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+            this.cccd = cccd;
+        }
+
+
+        info() {
+            return `Tôi tên là ${this.name}, 
+                    đang sống tại ${this.address},
+                    số điện thoại ${this.phone}, 
+                    số CMND là ${this.cccd}`;
+        }
+    };
+    class User extends Person {
+        constructor(name, address, phone, cccd, dateSalary) {
+            super(name, address, phone, cccd);
+            this.dateSalary = dateSalary;
+        }
+        info(){
+            return `Tôi tên là ${this.name}, 
+            đang sống tại ${this.address},
+            số điện thoại ${this.phone}, 
+            số CMND là ${this.cccd},
+            ngày ${this.dateSalary}`;
+        }
+    }
+    const thongTinUser = new User('qui', '123 tan binh', '0908732558', '12355678','5/12/2002');
+    console.log(thongTinUser.info());
+
+
+
+
+
+
+
+   
 
     return (
         <View>
