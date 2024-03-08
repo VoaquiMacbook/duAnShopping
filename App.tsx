47c44d7049850fac6050291ce57e6296b57d8b53
Vo-A-Qui <vohqui51202@icloud.com>
2024-03-08 12:56:54 +0700

    ok

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import type { PropsWithChildren } from 'react';
-import Detail from './src/apps/main/stacks/Detail';
-import Home from './src/apps/main/tabs/Home';
 import {
   SafeAreaView,
   ScrollView,
@@ -11,22 +9,8 @@ import {
   View,
   useColorScheme,
 } from 'react-native';
-import Shop from './src/apps/main/tabs/Home';
-import FlashSale from './src/an/FlashSale';
-import Start1 from './src/apps/authen/Start1';
-import Password from './src/apps/authen/Password';
-import Login from './src/apps/authen/Login';
-import Acccount from './src/apps/authen/Acccount';
-import Search from './src/apps/main/stacks/Search';
-import Dropdown from './src/apps/authen/Dropdown';
-// import voaqui
-import Profile from './src/apps/main/tabs/Profile';
-import Wishlist from './src/apps/main/tabs/Wishlist';
-import Cart from './src/apps/main/tabs/Cart';
-import Setting from './src/apps/main/tabs/Setting';
-import StackNavigatorMain from './src/voaqui/StackNavigatorMain';
-import AppNavigation from './src/apps/AppNavigation';
-import AuthenStackNavigation from './src/apps/authen/AuthenStackNavigation';
+import AppNavigation from './src/app/AppNavigation';
+import { AppProvider } from './src/app/main/AppContext';
 // import kiemTra JsNangCao
 import JsNangcao_kt_01 from './src/voaqui/data_JsNangCao/JsNangcao_kt_01';
 import The_Function from './src/voaqui/data_JsNangCao/The_Function';
@@ -43,32 +27,22 @@ import C8 from './src/voaqui/data_JsNangCao/Thi/C8';
 // react 2
 import HomePage from './src/voaqui/daNenTang2/HomePage';
 import Test_React2 from './src/voaqui/daNenTang2/test_react2';
+import Login from './src/app/authen/Login';
 function App(): React.JSX.Element {
-
-
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar barStyle="dark-content" backgroundColor="white" />
-      {/* <Cart /> */}
-      {/* <Profile/> */}
-      {/* <Wishlist/> */}
-      {/* <Setting /> */}
-      {/* <AuthenStackNavigation /> */}
-      {/* <JsNangcao_kt_01/> */}
-      {/* <The_Function/> */}
-      {/* <The_Object_Arr/> */}
-      {/* <Kt01_JsNangCao/> */}
-      {/* <Giai_De2 /> */}
-      {/* <Thi/> */}
-    {/* <C8/> */}
-    {/* <Test_React2/> */}
-    <HomePage/>
-    </SafeAreaView>
+    <AppProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar />
+        <AppNavigation />
+      </SafeAreaView>
+    </AppProvider>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+  },
 });
 
 export default App;
